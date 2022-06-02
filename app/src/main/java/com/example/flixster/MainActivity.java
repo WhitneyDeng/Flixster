f1aae1e5ad8bcf2e1f59f156ffa817a7a13c6b22
Whitney Deng <whitneyd@fb.com>
2022-06-02 15:37:50 -0700

    Added: - ViewHolder adapter & xml
    Issue:
    - tvs in viewholder cutoff
    - doesn't seem to be loading the data

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -1,18 +1,22 @@
 package com.example.flixster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.adapters.MovieAdapter;
 import com.example.flixster.models.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.Headers;
@@ -29,6 +33,17 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        movies = new ArrayList<>(); //instantiate movies
+
+        // create adapter
+        MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+
+        // set adapter on the recycler view
+        rvMovies.setAdapter(movieAdapter);
+
+        // set Layout Manager on the recycler view
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
 
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler()
@@ -42,14 +57,14 @@ public class MainActivity extends AppCompatActivity
                 {
                     JSONArray results = jsonObject.getJSONArray("results");
                     Log.i(TAG, "Results: " + results.toString());
-                    movies = Movie.fromJsonArray(results);
+                    movies.addAll(Movie.fromJsonArray(results));    //modify movies
+                    movieAdapter.notifyDataSetChanged();            //let adapter know to rerender rv
                     Log.i(TAG, "Movies: " + movies.size());
                 } catch (JSONException e)
                 {
                     Log.e(TAG, "Hit json exception", e);
                 }
             }
-            //testing git push
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable)
